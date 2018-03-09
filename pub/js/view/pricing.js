c0b4d7b4652925cb31d71bdb556ad26d9bbb7074
artyom.grishanov <grishanov.artem@gmail.com>
2018-03-09 21:43:51 +0300

    ++

--- pub/js/view/pricing.js
+++ pub/js/view/pricing.js
@@ -2,8 +2,14 @@
  * Created by artyom.grishanov on 01.03.18.
  */
 
-$('.js-pricing_rb').click(function(e){
+$('.js-pricing_rb_basic').click(function(e){
     var $e = $(e.currentTarget);
     var key = $e.attr('data-price-pts-key');
     $('.js-basic_tariff_price').html(App.getText(key));
+});
+
+$('.js-pricing_rb_business').click(function(e){
+    var $e = $(e.currentTarget);
+    var key = $e.attr('data-price-pts-key');
+    $('.js-business_tariff_price').html(App.getText(key));
 });
