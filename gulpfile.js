3f7bcb9a11e531ec6e822674f1005a7231e9a8ed
artyomgrishanov-qs <agrishanov@ambrahealth.com>
2020-07-20 20:29:40 +0300

    analytics

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@
  *          если я планирую склеить этот файл с общие стили style.css то по идее ссылка не нужна эта
  *
  */
-var productVersion = 'v2.1.18';
+var productVersion = 'v2.2.0';
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
